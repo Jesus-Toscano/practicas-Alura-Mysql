b39a28121d5baf4c285c8d21c9f1828ecdbaded6
Jesus-Toscano <132708915+Jesus-Toscano@users.noreply.github.com>
2023-10-26 11:07:23 -0600

    Add files via upload

---
+++ insertando_clientes.sql
@@ -0,0 +1,9 @@
+/*ERROR AL INSERTAR LOS VALORES, LIMITE DE CREDITO,VOLUMEN DE COMPRA,EDAD
+SE ESTABLECIERON COMO VARCHAR Y ERAN FLOAT*/
+
+
+INSERT INTO tb_clientes VALUES('9283760794','Edson Calisaya','Sta Ursula Xitla','Barrio del Niño Jesus','Ciudad de Mexico','EM','22002002','1995-01-07',25,'M',150000,250000,1),
+('7771579779','Marcelo Perez','F.C. De Cuernavaca 13','Carola','Ciudad de Mexico','EM','88202912','1992-01-25',29,'M',70000,160000,1),
+(' 5576228758',' Patricia Olivera',' Pachuca 75','Condesa','Ciudad de Mexico','EM','88192029',' 1995-01-14',25,'F',70000,160000,1);
+
+SELECT * FROM tb_clientes;
